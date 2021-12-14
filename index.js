53c49453fa70c16622acc0c6b3fe8027f73687ed
Bruna Campos <92brunacampos@gmail.com>
2021-12-14 16:25:06 -0300

    removing tests and files

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const PORT = 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
-
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
 app.use('/categories', categoriesRouter);
--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,5 @@
 require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     username: 'root',
-//     password: '8910',
-//     database: 'blogs_api',
-//     host: 'localhost',
-//     dialect: 'mysql',
-//   },
-//   test: {
-//     username: 'root',
-//     password: '8910',
-//     database: 'blogs_api',
-//     host: 'localhost',
-//     dialect: 'mysql',
-//   },
-//   production: {
-//     username: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: 'blogs_api',
-//     host: process.env.HOSTNAME,
-//     dialect: 'mysql',
-//   },
-// };
-
 module.exports = {
   development: {
     username: process.env.MYSQL_USER,
